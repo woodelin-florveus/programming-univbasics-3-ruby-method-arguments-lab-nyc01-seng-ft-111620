d30ab69016356644034c4d12325a9baa37eb28e7
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-21 23:10:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,7 +14,7 @@ def introduction_with_language(name, language)
 end
 
 def introduction_with_language_optional(name, language = "Ruby")
-puts puts "Hi, my name is #{name} and I am learning to program in #{language}."
+ puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
