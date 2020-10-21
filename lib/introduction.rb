e680283b26214dffaa9c08c4c66997f3044c1774
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-21 23:04:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,12 +3,12 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
-require 'pry'
+# require 'pry'
 
 def introduction(name)
 
 end
-binding.pry
+
 
 
 
